35a8515d297d71df147c28b397e40f578bde696f
kinoya <2877242250@qq.com>
2025-06-29 08:01:45 +0800

    音效，标题界面

--- src/component/sound_texture_btn/sound_texture_btn.gd
+++ src/component/sound_texture_btn/sound_texture_btn.gd
@@ -11,3 +11,19 @@ func _notification(p_what: int) -> void:
 	if p_what == NOTIFICATION_MOUSE_ENTER:
 		if hover_sound:
 			sound_bus.play_sound(hover_sound)
+var t
+func _ready() -> void:
+	mouse_entered.connect(
+		func():
+			t = create_tween()
+			t.tween_property(get_child(0),"scale:x",1.,0.3)\
+				.set_trans(Tween.TRANS_CUBIC)\
+				.set_ease(Tween.EASE_IN_OUT)
+	)
+	mouse_exited.connect(
+		func():
+			t = create_tween()
+			t.tween_property(get_child(0),"scale:x",0.,0.3)\
+				.set_trans(Tween.TRANS_CUBIC)\
+				.set_ease(Tween.EASE_IN_OUT)
+	)
--- src/scene/title/title_scene.gd
+++ src/scene/title/title_scene.gd
@@ -11,4 +11,5 @@ func _on_setting_btn_pressed() -> void:
 
 
 func _on_exit_btn_pressed() -> void:
+	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
