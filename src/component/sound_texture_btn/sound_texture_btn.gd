151201e98aaaf042165753043a250ccb58673e74
kinoya <2877242250@qq.com>
2025-06-29 10:14:01 +0800

    标题界面更新

--- src/component/sound_texture_btn/sound_texture_btn.gd
+++ src/component/sound_texture_btn/sound_texture_btn.gd
@@ -13,6 +13,7 @@ func _notification(p_what: int) -> void:
 			sound_bus.play_sound(hover_sound)
 var t
 func _ready() -> void:
+	get_child(0).scale.x = 0.000001
 	mouse_entered.connect(
 		func():
 			t = create_tween()
@@ -23,7 +24,7 @@ func _ready() -> void:
 	mouse_exited.connect(
 		func():
 			t = create_tween()
-			t.tween_property(get_child(0),"scale:x",0.,0.3)\
+			t.tween_property(get_child(0),"scale:x",0.000001,0.3)\
 				.set_trans(Tween.TRANS_CUBIC)\
 				.set_ease(Tween.EASE_IN_OUT)
 	)
